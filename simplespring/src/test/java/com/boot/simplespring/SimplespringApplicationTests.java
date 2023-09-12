f81682a00c5f81c05b949496dc47b6d73c51c56d
Bhara-1234 <104549544+Bhara-1234@users.noreply.github.com>
2023-09-12 16:58:59 +0530

    Add files via upload

---
+++ simplespring/src/test/java/com/boot/simplespring/SimplespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.simplespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
